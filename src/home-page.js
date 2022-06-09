4ce8f8e76d5d682f3999ad4a6ceab819b148892d
Katelyn Lindberg <katelynhouston@live.com>
2022-06-08 18:16:30 -0600

    attempts at DOM manip for HTML setup

--- src/home-page.js
+++ src/home-page.js
@@ -1,46 +1,41 @@
-function domSetup() {
+export function domSetup() {
     const content = document.getElementById('content');
     
     const header = document.createElement('header');
-    const logo = document.createElement('div');
-    const logoImg = document.createElement('img');
-    const navList = document.createElement('div');
-    const navUL = document.createElement('ul');
-    const listText = ['Menu', 'Contact', 'Yelp'];
-    for (let i = 0; i < listText.length; i++) {
-        const listItem = document.createElement('li');
-        const listLink = document.createElement('a');
-        listLink.textContent = listText[i];
-        listItem.appendChild(listLink);
-        navUL.appendChild(listItem);
-    };
 
+    const logo = document.createElement('div');
+    logo.innerHTML = '<img src="../src/img/logo.png" alt="Kanpai Home">'
+    logo.classList.add('logo');    
+    header.appendChild(logo);
+
+    const navList = document.createElement('div');
+    navList.classList.add('nav-list');
+    const navUL = document.createElement('ul');
+    const li1 = document.createElement('li'), li2 = document.createElement('li'), li3 = document.createElement('li');
+    li1.innerHTML = '<a href="#">Menu</a>';
+    li2.innerHTML = '<a href="#">Contact</a>';
+    li3.innerHTML = '<a href="#">Yelp</a>';
+    navUL.appendChild(li1, li2, li3);
+    navList.appendChild(navUL);
+    header.appendChild(navList);
+    
     const hero = document.createElement('div');
     const main = document.createElement('div');
     const title = document.createElement('p');
     const empList = ['Manager Name', 'Lead Chef Name', 'Sous Chef Name', 'Lead Server Name'];
     for (let i = 0; i < empList.length; i++) {
-        const p1 = document.createElement('div');
-        const teamMember = document.createElement('p');
-        const teamPic = document.createElement('img');
-        p1.classList.add('p1');
-        teamMember.classList.add('team-member');
-        teamPic.classList.add('team-pic');
-        p1.append(teamMember, teamPic);
-        main.appendChild(p1);
+        for (let i = 0; i < main.length; i++) {
+            const p1 = document.createElement('div');
+            const teamMember = document.createElement('p');
+            const teamPic = document.createElement('img');
+            p1.classList.add('p1');
+            teamMember.classList.add('team-member');
+            teamPic.classList.add('team-pic');
+            p1.append(teamMember, teamPic);
+            main.append(p1);
+        }
     }
     
-
-    logo.classList.add('logo');
-    logoImg.setAttribute('id', 'logo-img');
-    navList.classList.add('nav-list');
-    logo.appendChild(logoImg);
-    navList.appendChild(navUL);
-    header.appendChild(logo);
-
-    main.appendChild(title);
-    hero.appendChild(main);
     content.appendChild(header);
-    content.appendChild(hero);
 }
 domSetup();
