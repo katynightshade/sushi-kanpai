f8a67f17ab876a392d13012c10e10c911a887f55
Katelyn Lindberg <katelynhouston@live.com>
2022-06-10 09:37:00 -0600

    mennu.js template setup

---
+++ src/menu.js
@@ -0,0 +1,26 @@
+export function menuSetup() {
+    const content = document.querySelector('#content');
+    
+    const header = document.createElement('div');
+    header.classList.add('header');
+
+    const logo = document.createElement('div');
+    logo.innerHTML = '<img src="../src/img/logo.png" alt="Kanpai Home">'
+    logo.classList.add('logo');    
+    header.appendChild(logo);
+
+    const navList = document.createElement('div');
+    navList.classList.add('nav-list');
+    const navUL = document.createElement('ul');
+    const li1 = document.createElement('li'), li2 = document.createElement('li'), li3 = document.createElement('li');
+    li1.innerHTML = '<a href="#">Menu</a>';
+    li2.innerHTML = '<a href="#">Contact</a>';
+    li3.innerHTML = '<a href="#">Yelp</a>';
+    navUL.append(li1, li2, li3);
+    navList.appendChild(navUL);
+    header.appendChild(navList);
+
+    const hero = document.createElement('div');
+    const main = document.createElement('div');
+    //create dom elements for menu items, append all to content
+}
